4a90303ae67b60890e1db35819a77cd841c037fb
YusupovZA <jusupovz@gmail.com>
2020-11-25 23:12:05 +0500

    init templates

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import Home from '../views/Home.vue'
+import MainPage from '../views/MainPage.vue'
 import Template from '../views/Template.vue'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
@@ -9,7 +10,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: MainPage
   },
   {
     path: '/template/:code',
--- src/views/Template.vue
+++ src/views/Template.vue
@@ -1,19 +1,38 @@
 <template>
-  <div>123</div>
+  <v-app>
+    <v-container fill-height fluid>
+      <div class="text-center" v-if="loading">
+        <v-progress-circular
+          :size="50"
+          indeterminate
+          color="red"
+        ></v-progress-circular>
+      </div>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 import appTypes from '../store/app/types'
+import { mapActions, mapMutations, mapState } from 'vuex'
 export default {
-  data: () => ({
-    test: 'sss'
-  }),
+  computed: {
+    ...mapState({
+      loading: state => state.app.loading
+    })
+  },
+  methods: {
+    ...mapMutations('app', [appTypes.SET_LOADING_APP]),
+    ...mapActions('app', [appTypes.INIT_TEMPLATE])
+  },
   mounted() {
     console.log(this.$route.params)
-    this.$store.dispatch(
-      `app/${appTypes.INIT_TEMPLATE}`,
-      this.$route.params.code
-    )
+    this[appTypes.SET_LOADING_APP](true)
+    this[appTypes.INIT_TEMPLATE](this.$route.params.code).then(() => {
+      setTimeout(() => {
+        this.$router.push('/')
+      }, 2000)
+    })
   }
 }
 </script>
