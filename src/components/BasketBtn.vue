f2eeebef2c0b3cc441b632cf41836103b18a5618
YusupovZA <jusupovz@gmail.com>
2020-12-22 16:21:22 +0500

    temp

---
+++ src/components/BasketBtn.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-btn
+    fab
+    small
+    width="48"
+    height="48"
+    class="mloyalty-circle-basket"
+    elevation="0"
+    @click.prevent="openShopCart"
+  >
+    <img
+      class="shop-cart-icon"
+      src="~@/assets/img/default/shopping-cart.png"
+      alt=""
+    />
+    <span class="shop-cart-count">{{ allPositions.count }}</span>
+  </v-btn>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters('basket', ['allPositions'])
+  },
+  methods: {
+    openShopCart() {
+      this.$router.push('/basket')
+    }
+  }
+}
+</script>
